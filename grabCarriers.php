64652a13c6542812869a689c9b14d5ccfd42e33b
riscb <brischbieter@primeinc.com>
2019-12-03 09:41:46 -0600

    changed query for grabbing carriers

--- grabCarriers.php
+++ grabCarriers.php
@@ -5,7 +5,7 @@ $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $sqlGrabCarriers = "SELECT LBCARR from EDGETSTLIB.LOAPPBRK where LBCARR = 'RUMEN' or LBCARR = 'MMMBBQ' or LBCARR = 'IMGLLC'
 or LBCARR = 'A&DFRE'";
 
-$sqlGrabCarriers = "SELECT LBCARR from EDGETSTLIB.LOAPPBRK";
+$sqlGrabCarriers = "select distinct(LTPRMCAR) as LBCARR from edgetstlib.loapptrk";
 
 $getCarriers = $con->prepare($sqlGrabCarriers);
 $getCarriers->execute();
